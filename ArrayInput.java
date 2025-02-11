fc6daa41b5c685a1d40bf43785503a8c38769997
Naman Mittal <mittalnaman2005@gmail.com>
2025-02-11 11:54:01 +0530

    Creating ArrayInput constructor

--- ArrayInput.java
+++ ArrayInput.java
@@ -2,4 +2,9 @@ import java.util.*;
 
 class ArrayInput {
   
+    private Scanner scan;
+    // Constructor accepts an already created Scanner object
+    public ArrayInput(Scanner scan) {
+        this.scan = scan;
+    }
 }
