64a43bfa7633b4b42c0a1018fc7e380a08572113
Naman Mittal <mittalnaman2005@gmail.com>
2025-02-11 11:58:50 +0530

    Creating oddEven method
    Creating method to separate odd and even numbers from the array list

--- ArrayFunctions.java
+++ ArrayFunctions.java
@@ -2,13 +2,22 @@
 import java.util.*;
 
 class ArrayFunctions {
-  
     // Display an ArrayList of integers
     void display(ArrayList<Integer> array) {
         System.out.println("ArrayList: " + array);
     }
+
     // Display an integer array
     void display(int[] array) {
         System.out.println("Array: " + Arrays.toString(array));
+    }
+    // Separate and display odd and even numbers from an array
+    void oddEven(int[] array) {
+        ArrayList<Integer> even = new ArrayList<>();
+        ArrayList<Integer> odd = new ArrayList<>();
+        for (int num : array) {
+            if (num % 2 == 0) even.add(num); // Add even numbers
+            else odd.add(num); // Add odd numbers
+        }
     }
 }
