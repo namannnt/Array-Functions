d4e04a1a2a9588bd68e2ca7906873deb04c704b0
Naman Mittal <mittalnaman2005@gmail.com>
2025-02-11 11:58:08 +0530

    Create methods for Display of Results
    1 method for the display of the ArrayList and 2nd method for the display of Array

--- ArrayFunctions.java
+++ ArrayFunctions.java
@@ -3,4 +3,12 @@ import java.util.*;
 
 class ArrayFunctions {
   
+    // Display an ArrayList of integers
+    void display(ArrayList<Integer> array) {
+        System.out.println("ArrayList: " + array);
+    }
+    // Display an integer array
+    void display(int[] array) {
+        System.out.println("Array: " + Arrays.toString(array));
+    }
 }
